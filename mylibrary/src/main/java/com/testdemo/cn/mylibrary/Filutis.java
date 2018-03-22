91cc8fe4e1e58183a8ebbb026b2f878ddf3f06a6
zhangi789 <www.392032143@qq.com>
2018-03-22 17:17:37 +0800

    Initial commit

---
+++ mylibrary/src/main/java/com/testdemo/cn/mylibrary/Filutis.java
@@ -0,0 +1,16 @@
+package com.testdemo.cn.mylibrary;
+
+/**
+ * @author 张海洋
+ * @Date on 2018/03/22.
+ * @org 上海相舆科技有限公司
+ * @describe
+ */
+
+public class Filutis {
+
+
+    public int   add(int sunm,int ddfd){
+        return  sunm+ddfd;
+    }
+}
